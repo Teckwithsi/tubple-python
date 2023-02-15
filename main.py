3f46cfaf761faa2b6a155eb617f2a57c589dd036
simon <124110714+Teckwithsi@users.noreply.github.com>
2023-02-15 12:09:54 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+# a tuple ordered and allows duplicate elements
+
+
+
+mytuple = "max", 28 , "boston "
+
+
+print(mytuple)
+
+
+# always add acomma thingy to make it a tuple if only one item
+
+
+mytuple = ("max",)
+
+print(type(mytuple))
+
+
+# so now is a tuple :)
+
+mytuple = tuple
+
+
+
+student = ("bro", 21 , "male")
+
+
+print(student.count("bro"))
+
+
+for x in student:
+    print(x)
+
+if "bro" in student:
+    print("bro is here")
+
+
+
+
+
+
+
+
+
